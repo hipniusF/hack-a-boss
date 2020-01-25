dd03f23371a39d819f6667047770427fe30f8094
hipnius_f <marcopg4@gmail.com>
2020-01-25 21:38:23 +0100

    Switch-case method

--- js/calculadoraSimple/www/js/src.js
+++ js/calculadoraSimple/www/js/src.js
@@ -5,16 +5,42 @@ let operation = prompt("Ingresa tu operacióin (suma, multiplicación, división
 let number1 = +prompt("Ingresa tu primer número:");
 let number2 = +prompt("Ingresa tu segundo número:");
 
-if(operation === "suma") {
-  console.log(number1 + number2);
-} else if (operation === "multiplicación"){
-  console.log(number1*number2);
-} else if(operation === "división"){
-  console.log(number1/number2);
-} else if (operation === "resta"){
-  console.log(number1-number2);
-} else if (operation === "resto"){
-  console.log(number1%number2);
-} else{
-  console.log("Operación no reconocida, comprueba la sintaxis.")
+// if(operation === "suma") {
+//   console.log(number1 + number2);
+// } else if (operation === "multiplicación"){
+//   console.log(number1*number2);
+// } else if(operation === "división"){
+//   console.log(number1/number2);
+// } else if (operation === "resta"){
+//   console.log(number1-number2);
+// } else if (operation === "resto"){
+//   console.log(number1%number2);
+// } else{
+//   console.log("Operación no reconocida, comprueba la sintaxis.")
+// }
+
+switch (operation) {
+  case "suma":
+    console.log(number1 + number2);
+    break;
+  
+  case "multiplicación":
+    console.log(number1*number2);
+    break;
+  
+  case "división":
+    console.log(number1/number2);
+    break;
+
+  case "resta":
+    console.log(number1-number2);
+    break;
+
+  case "resto":
+    console.log(number1%number2);
+    break;
+
+  default:
+    console.log("Operación no reconocida, comprueba la sintaxis.")
+    break;
 }
