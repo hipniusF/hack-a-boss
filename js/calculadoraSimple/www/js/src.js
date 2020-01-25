71f347dd2655464aabcb99856431c0c148805188
Marco <marcopg4@gmail.com>
2020-01-25 20:56:47 +0100

    Creación calculadora simple

---
+++ js/calculadoraSimple/www/js/src.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let divisa = 'rupia';
+
+if (divisa === 'dolares') {
+  console.log('1 dolar son 0.91€');
+} else if (divisa === 'libra') {
+  console.log('1 libra son 1.19€');
+} else if (divisa === 'rupia') {
+  console.log('1 rupia son 0.013€');
+}
