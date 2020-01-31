e391e810c91d9811e1ad99900f8c34e41f35eeb0
Marco <marcopg4@gmail.com>
2020-01-31 18:29:02 +0100

    JS Día 5

---
+++ js/workingDirectory/www/js/recursion.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// function factorial(numberToShout) {
+//   if (numberToShout === 1 || numberToShout === 0) {
+//     return 1;
+//   }
+//   return factorial(numberToShout - 1) * numberToShout;
+// }
+// console.log(factorial(5));
+
+// function badFactorial(numberToShout) {
+//   if (numberToShout === 0) {
+//     return 1; //MAGIC NUMBER, WARNING!!!
+//   }
+//   let result = numberToShout;
+//   for (let i = numberToShout - 1; i > 1; i--) {
+//     result = result * i;
+//   }
+//   return result;
+// }
+
+// console.log(badFactorial(5);
+
+function fib(numberToFib) {
+  if (numberToFib <= 1) {
+    return numberToFib;
+  }
+  return fib(numberToFib - 1) + fib(numberToFib - 2);
+}
+console.log(fib(87));
--- js/workingDirectory/www/js/src.js
+++ js/workingDirectory/www/js/src.js
@@ -1,35 +1,29 @@
 'use strict';
 
-// function factorial(numberToShout) {
-//   if (numberToShout === 1 || numberToShout === 0) {
-//     return 1;
-//   }
-//   return factorial(numberToShout - 1) * numberToShout;
-// }
-// console.log(factorial(5));
-
-// function badFactorial(numberToShout) {
-//   if (numberToShout === 0) {
-//     return 1; //MAGIC NUMBER, WARNING!!!
-//   }
-//   let result = numberToShout;
-//   for (let i = numberToShout - 1; i > 1; i--) {
-//     result = result * i;
-//   }
-//   return result;
-// }
-
-// console.log(badFactorial(5);
-
-function fib(numberToFib) {
-  let result = 0;
-  if (numberToFib === 0) {
-    return result;
-  } else if (numberToFib === 1) {
-    result = 1;
-    return result;
+/*const yo = {
+  name: 'Marco',
+  address: {
+    street: 'Los asdfasdfasdf',
+    houseNumber: '37',
+    city: '42',
+    country: 'spain'
+  },
+  age: 16,
+  pets: null,
+  adult: function() {
+    return this.age > 18;
+  },
+  parents: {
+    father: 'asdfadf',
+    mother: 'asdfasdf'
   }
-  result = fib(numberToFib - 1) + fib(numberToFib - 2);
-  return result;
-}
-console.log(fib(8));
+};
+
+const yoCopy = { ...yo };
+console.log(yoCopy === yo);*/
+
+const test = {};
+test.a = {};
+test.b = test.a;
+
+console.log(test);
