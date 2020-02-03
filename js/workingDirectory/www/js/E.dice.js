b78323ae7b2ebc0d079000d289ef46999d9f6166
Marco <marcopg4@gmail.com>
2020-02-03 20:43:06 +0100

    E. dice

---
+++ js/workingDirectory/www/js/E.dice.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let point = 0;
+let roll = 0;
+let scoreToWin = 50;
+
+function dice() {
+  for (let i = 0; point < scoreToWin; i++) {
+    roll = Math.ceil(Math.random() * 6);
+    point += roll;
+
+    console.log(`Tirada ${i}: ha salido un ${roll}. Tienes un total de ${point} puntos`);
+  }
+
+  console.log(`¡Enhorabuena, ha salido un ${roll}! ¡Has ganado con un total de ${point} puntos!`);
+}
+
+dice();
