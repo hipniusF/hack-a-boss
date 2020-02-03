03c196a7bdc531c26181a8de14d9fb03404628d7
Marco <marcopg4@gmail.com>
2020-02-03 21:12:52 +0100

    T. arrays CallBack Functions

---
+++ js/workingDirectory/www/js/T.arraysCallBackFunctions.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const arr = [5, 8, 7, 6, 2, 4];
+
+const increased = arr.map((value) => value + 2);
+
+console.log(increased);
+
+for (const value of arr) {
+  if (value === 10) {
+    break;
+  }
+}
+
+const greaterThanFive = arr.filter((value) => value >= 5);
+
+const reducer = (accumulator, currentValue) => {
+  return accumulator + currentValue;
+};
+
+const reducedArray = arr.reduce(reducer);
+
+console.log(reducedArray);
