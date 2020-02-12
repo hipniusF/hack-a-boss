df9e9dfaa81010c8e567ef170250f5f55173189e
Marco <marcopg4@gmail.com>
2020-02-12 21:25:00 +0100

    E. promise

---
+++ js/workingDirectory/www/js/E.Promise.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const myPass = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('134');
+  }, Math.random() * 1000);
+});
+
+const serverHash = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('1234');
+  }, Math.random * 1000);
+});
+
+const promiseArray = [myPass, serverHash];
+
+Promise.all(promiseArray).then((passwords) => {
+  if (passwords[0] === passwords[1]) {
+    console.log('Access granted');
+  } else {
+    console.log('Access denied');
+  }
+});
