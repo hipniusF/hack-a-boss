e200cd6df69a85fd05d9aaef8024f9061f3f9a96
Marco <marcopg4@gmail.com>
2020-03-04 20:02:15 +0100

    JS2 Day 4 Exercise Improve

--- js2/dia-4/www/js/exercise.js
+++ js2/dia-4/www/js/exercise.js
@@ -1,23 +1,38 @@
 'use strict';
 
-const button = document.querySelector('h1');
+const button = document.querySelector('button');
+const body = document.body;
 
 button.addEventListener('mouseover', handlerMove);
-document.addEventListener('keypress', stopMovement);
+button.addEventListener('click', handlerClick);
+body.addEventListener('dblclick', handlerStopMovement);
+
+function handlerClick() {
+	const winText = document.createElement('h1');
+	winText.textContent = 'CONGRATULATIONS!!!!';
+	winText.style.fontSize = '5rem';
+	body.append(winText);
+
+	button.setAttribute('hidden', 'yes');
+
+	setTimeout(reload, 2000);
+}
 
 function handlerMove() {
-	button.style.top = `${randomNumber(window.innerHeight - 150)}px`;
-	button.style.left = `${randomNumber(window.innerWidth - 100)}px`;
+	button.style.top = `${randomNumber(window.innerHeight - 100)}px`;
+	button.style.left = `${randomNumber(window.innerWidth - 150)}px`;
+}
+
+function handlerStopMovement(event) {
+	if (event.ctrlKey) {
+		button.removeEventListener('mouseover', handlerMove);
+	}
 }
 
 function randomNumber(max) {
 	return Math.round(Math.random() * max);
 }
 
-function stopMovement() {
-	button.removeEventListener('mouseover', handlerMove);
-}
-
-function keylog(key) {
-	stopMovement();
+function reload() {
+	window.location.reload(false);
 }
