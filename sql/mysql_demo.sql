a3fc76f4100ffb0534d5abee7cacf3fad0b3d5b1
hipnius_f <marcopg4@gmail.com>
2020-03-07 14:57:50 +0100

    SQL Day 1

---
+++ sql/mysql_demo.sql
@@ -0,0 +1,51 @@
+-- Thanks to http://justinsomnia.org/2009/04/the-employee-and-department-tables-for-mysql/
+
+-- Run as demo user: mysql -u demo -ppassword < mysql_employee_department.sql
+
+CREATE DATABASE IF NOT EXISTS hb_employees;
+USE hb_employees;
+
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name  varchar(14) default NULL,
+  location varchar(13) default NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  name varchar(10) default NULL,
+  job varchar(9) default NULL,
+  manager_id INT default NULL,
+  hire_date date default NULL,
+  salary decimal(7,2) default NULL,
+  commission decimal(7,2) default NULL,
+  department_id INT default NULL, 
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+#alter table employee add constraint fk_manager foreign key (manager_id) references employee(id);
+
+INSERT INTO department VALUES ('10','ACCOUNTING','NEW YORK');
+INSERT INTO department VALUES ('20','RESEARCH','DALLAS');
+INSERT INTO department VALUES ('30','SALES','CHICAGO');
+INSERT INTO department VALUES ('40','OPERATIONS','BOSTON');
+
+INSERT INTO employee VALUES ('7369','SMITH','CLERK','7902','1980-12-17','800.00',NULL,'20');
+INSERT INTO employee VALUES ('7499','ALLEN','SALESMAN','7698','1981-02-20','1600.00','300.00','30');
+INSERT INTO employee VALUES ('7521','WARD','SALESMAN','7698','1981-02-22','1250.00','500.00','30');
+INSERT INTO employee VALUES ('7566','JONES','MANAGER','7839','1981-04-02','2975.00',NULL,'20');
+INSERT INTO employee VALUES ('7654','MARTIN','SALESMAN','7698','1981-09-28','1250.00','1400.00','30');
+INSERT INTO employee VALUES ('7698','BLAKE','MANAGER','7839','1981-05-01','2850.00',NULL,'30');
+INSERT INTO employee VALUES ('7782','CLARK','MANAGER','7839','1981-06-09','2450.00',NULL,'10');
+INSERT INTO employee VALUES ('7788','SCOTT','ANALYST','7566','1982-12-09','3000.00',NULL,'20');
+INSERT INTO employee VALUES ('7839','KING','PRESIDENT',NULL,'1981-11-17','5000.00',NULL,'10');
+INSERT INTO employee VALUES ('7844','TURNER','SALESMAN','7698','1981-09-08','1500.00','0.00','30');
+INSERT INTO employee VALUES ('7876','ADAMS','CLERK','7788','1983-01-12','1100.00',NULL,'20');
+INSERT INTO employee VALUES ('7900','JAMES','CLERK','7698','1981-12-03','950.00',NULL,'30');
+INSERT INTO employee VALUES ('7902','FORD','ANALYST','7566','1981-12-03','3000.00',NULL,'20');
+INSERT INTO employee VALUES ('7934','MILLER','CLERK','7782','1982-01-23','1300.00',NULL,'10');
