9df413e0f60e690ac1f7d69eedcbe5ca17757080
hipnius <marcopg4@gmail.com>
2020-05-08 18:42:55 +0200

    Travel diary MySQL exercise

---
+++ backend/diaro-viaje-MySQL/controllers/validations/index.js
@@ -0,0 +1,42 @@
+const Joi = require('@hapi/joi');
+
+// New entry
+const entrySchema = Joi.object().keys({
+  place: Joi.string()
+    .max(100)
+    .required()
+    .error(
+      new Error(
+        'El campo place es obligatorio y no puede tener más de 100 caracteres'
+      )
+    ),
+  description: Joi.string()
+    .max(1000)
+    .required()
+    .error(
+      new Error(
+        'El campo description es obligatorio y non puede tener más de 1000 caracteres'
+      )
+    )
+});
+
+const voteSchema = Joi.object().keys({
+  vote: Joi.number()
+    .min(1)
+    .max(5)
+    .integer()
+    .required()
+    .error(new Error('El campo voto debe existir y ser un número entre 1 y 5'))
+});
+
+const searchSchema = Joi.string()
+  .max(30)
+  .error(
+    new Error('El campo de búsqueda debe tiene un máximo de 30 caracteres')
+  );
+
+module.exports = {
+  entrySchema,
+  voteSchema,
+  searchSchema
+};
