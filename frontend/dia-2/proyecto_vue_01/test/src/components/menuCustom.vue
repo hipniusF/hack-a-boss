717f5240d85ef63971ef36cdca7107ab0bdf1237
hipnius <marcopg4@gmail.com>
2020-05-28 22:02:07 +0200

    Frontend day 2

---
+++ frontend/dia-2/proyecto_vue_01/test/src/components/menuCustom.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "menuCustom",
+  props: {}
+};
+</script>
+
+<style scoped>
+</style>
