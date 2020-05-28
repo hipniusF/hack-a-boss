983016afd45a277a93998064f7e8bd188da9aa14
hipnius <marcopg4@gmail.com>
2020-05-28 22:02:07 +0200

    Frontend day 2

---
+++ frontend/dia-2/proyecto_vue_01/test/src/components/footerCustom.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <footer>
+      <p>(c) Marco Pérez González 2020</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footerCustom",
+  props: {}
+};
+</script>
+
+<style scoped>
+footer {
+  color: white;
+  background: rgb(51, 51, 51);
+  position: absolute;
+  width: 99%;
+  bottom: 0;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ frontend/dia-2/proyecto_vue_01/test/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <menuCustom></menuCustom>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <footerCustom></footerCustom>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import menuCustom from "@/components/menuCustom.vue";
+import footerCustom from "@/components/footerCustom.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    menuCustom,
+    footerCustom
+  }
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+div.home {
+  padding: 0;
+  min-height: 98vh;
+}
+</style>
